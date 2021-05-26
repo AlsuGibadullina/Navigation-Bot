3af2daa0a783569350cb02eb3051dcad4c872e1e
etoal <alsuwinx@mail.ru>
2021-05-26 15:59:50 +0300

    add new command

--- bot.py
+++ bot.py
@@ -11,17 +11,25 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+    await message.reply("Привет!\nЭто бот для навигации по интсрументам наставничества")
 
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+    await message.reply("Список команд: \n /start - старт работы \n /exit - завершение работы\n /help - помощь ")
+
+
+@dp.message_handler(commands=['exit'])
+async def process_exit_command(message: types.Message):
+    await message.reply("Спасибо за обращение!")
 
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
+
+
+
 
 
 if __name__ == '__main__':
