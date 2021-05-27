9ac95e9aff75e79024cc8f98da2fc4e8e71d6230
Alina <zikibikk@gmail.com>
2021-05-27 10:25:25 +0300

    added table, doc, form buttons

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+import keyboard as kb
+
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
@@ -10,17 +12,16 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nЭто бот для навигации по интсрументам наставничества")
+    await message.reply("Что вас интересует?", reply_markup=kb.start_kb)
 
 
-@dp.message_handler(commands=['kaef'])
-async def process_start_command(message: types.Message):
-    await message.reply("Кааааййййффффф")
-
+@dp.message_handler(commands=['theme'])
+async def process_example_command(message: types.Message):
+    await message.reply("Список подпунктов ")
 
 @dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
-    await message.reply("Список команд: \n /start - старт работы \n /exit - завершение работы\n /help - помощь ")
+    await message.reply("Список команд: \n /theme  /start - старт работы \n /exit - завершение работы\n /help - помощь ")
 
 
 @dp.message_handler(commands=['exit'])
@@ -31,9 +32,6 @@ async def process_exit_command(message: types.Message):
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
-
-
-
 
 
 if __name__ == '__main__':
--- keyboard.py
+++ keyboard.py
@@ -2,3 +2,8 @@ from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
+button_tables = KeyboardButton('Таблицы')
+button_docs = KeyboardButton('Документы')
+button_forms = KeyboardButton('Гугл Формы')
+
+start_kb = ReplyKeyboardMarkup().add(button_tables).add(button_docs).add(button_forms)
